8042efd68bb30e74e5a10ffb82e6db52e9468755
cm8908 <9781689+cm8908@users.noreply.github.com>
2020-08-24 19:19:31 +0900

    vertically written output / TL func revised
    current: matching characters one by one through 'for' loop
    
    direction: comparing two arrays and match each of their elements' index number

--- site/transliterator/js/script.js
+++ site/transliterator/js/script.js
@@ -3,11 +3,32 @@ document.addEventListener("DOMContentLoaded",
 
 		function TL (event) {
 			var inputText = document.getElementById("input").value;
-			var outputText = inputText + "ed";
+			var outputText = new Array();
+
+			var latin = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+			for (var i = 0; i < inputText.length; i++) {
+				if (inputText[i] == "a") {
+					outputText[i] = "1";
+				}
+				else if (inputText[i] == "b") {
+					outputText[i] = "2"
+				}
+				else {
+					outputText[i] = "x"
+				}
+			}
+
+			var result = "";
+
+			for (i = 0; i < outputText.length; i++) {
+				result += outputText[i];
+			}
+			console.log(result);
 
 
 			document.getElementById("OutputTextBox")
-					.textContent = outputText;
+					.textContent = result;
 
 		}
 
